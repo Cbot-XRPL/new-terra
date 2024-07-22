124dd1071e9ab2ee372c78c968816ae4885b020b
Cbot-XRPL <cybrcris@gmail.com>
2024-07-22 17:51:48 -0400

    local update

--- script.js
+++ script.js
@@ -2,37 +2,40 @@
 
 console.log('page refresh')
 
+//let local = "http://127.0.0.1:5500"
+let local = "http://192.168.1.163:8082"
+
 
 
 
 //routing buttons ----------------------------------------------------------------------------------------------------
 
 let logoButton = () => {
-  location.assign("http://127.0.0.1:5500/index.html")
+  location.assign(local)
   console.log('open active menu')
   }
 
 let menuButton = () => {
-location.assign("http://127.0.0.1:5500/index.html#1")
+location.assign(`${local}/index.html#1`)
 console.log('open active menu')
 }
 
 
 let serviceButton = async () => {
-location.assign("http://127.0.0.1:5500/index.html#1")
+location.assign(`${local}/index.html#1`)
 console.log('go to services')
 }
 
 
 
 let aboutButton = () => {
-location.assign("http://127.0.0.1:5500/index.html#1")
+location.assign(`${local}/index.html#1`)
 console.log('go to about us')
 }
 
 
 let faqButton = () => {
-location.assign("http://127.0.0.1:5500/index.html#1")
+location.assign(`${local}/index.html#1`)
 console.log('go to FAQ')
 }
 
